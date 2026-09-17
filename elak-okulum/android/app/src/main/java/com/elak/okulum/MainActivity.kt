3a621d26670a6cdf8406d3aa2b1cb22228b0eaf4
samins82-cloud <samins82@gmail.com>
2026-09-17 12:28:05 +0300

    Fix Turkish UTF-8 and system bar overlap in ELAK Okulum

--- elak-okulum/android/app/src/main/java/com/elak/okulum/MainActivity.kt
+++ elak-okulum/android/app/src/main/java/com/elak/okulum/MainActivity.kt
@@ -2,14 +2,21 @@ package com.elak.okulum
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import android.webkit.CookieManager
 import android.webkit.WebChromeClient
 import android.webkit.WebResourceRequest
+import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     companion object {
@@ -27,8 +34,32 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Türkçe yerel ayarı: WebView/PHP tarih-sayı gösterimlerinde tutarlılık sağlar.
+        Locale.setDefault(Locale.forLanguageTag("tr-TR"))
+
+        // Android 15+ edge-to-edge davranışında içeriğin saat, kamera deliği ve
+        // alt gezinme çubuğunun altında kalmaması için sistem boşluklarını biz yönetiyoruz.
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.statusBarColor = Color.rgb(8, 26, 56)
+        window.navigationBarColor = Color.WHITE
+
         webView = WebView(this)
+        webView.setBackgroundColor(Color.WHITE)
         setContentView(webView)
+
+        val insetsController = WindowInsetsControllerCompat(window, webView)
+        insetsController.isAppearanceLightStatusBars = false
+        insetsController.isAppearanceLightNavigationBars = true
+
+        ViewCompat.setOnApplyWindowInsetsListener(webView) { view, insets ->
+            val bars = insets.getInsets(
+                WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout()
+            )
+            view.setPadding(bars.left, bars.top, bars.right, bars.bottom)
+            insets
+        }
+        ViewCompat.requestApplyInsets(webView)
 
         CookieManager.getInstance().setAcceptCookie(true)
         CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true)
@@ -40,7 +71,10 @@ class MainActivity : AppCompatActivity() {
             allowFileAccess = false
             allowContentAccess = false
             loadsImagesAutomatically = true
-            userAgentString = "$userAgentString ELAK-Okulum/0.4"
+            defaultTextEncodingName = "UTF-8"
+            textZoom = 100
+            cacheMode = WebSettings.LOAD_DEFAULT
+            userAgentString = "$userAgentString ELAK-Okulum/0.4.1 tr-TR"
         }
 
         webView.webChromeClient = WebChromeClient()
@@ -60,7 +94,11 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-        webView.loadUrl(BASE_URL)
+
+        webView.loadUrl(
+            BASE_URL,
+            mapOf("Accept-Language" to "tr-TR,tr;q=0.9,en;q=0.6")
+        )
     }
 
     override fun onResume() {
