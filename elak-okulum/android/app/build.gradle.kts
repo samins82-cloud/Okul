44ed22a9998b2e968c1464405a77b28b9bbee4d5
samins82-cloud <samins82@gmail.com>
2026-09-17 10:49:59 +0300

    Add ELAK Okulum app module

---
+++ elak-okulum/android/app/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+
+android {
+    namespace = "com.elak.okulum"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.elak.okulum"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 3
+        versionName = "0.3.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+}
