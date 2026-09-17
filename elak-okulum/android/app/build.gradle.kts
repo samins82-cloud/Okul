aa33f77d8f2b2c84deef01940163fabf008c1151
samins82-cloud <samins82@gmail.com>
2026-09-17 11:03:14 +0300

    Align ELAK Okulum Java and Kotlin targets to 17

--- elak-okulum/android/app/build.gradle.kts
+++ elak-okulum/android/app/build.gradle.kts
@@ -3,12 +3,22 @@ plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 android {
     namespace = "com.elak.okulum"
     compileSdk = 35
+
     defaultConfig {
         applicationId = "com.elak.okulum"
         minSdk = 24
         targetSdk = 35
         versionCode = 3
         versionName = "0.3.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
 
