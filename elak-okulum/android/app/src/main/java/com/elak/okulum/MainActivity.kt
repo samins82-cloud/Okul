f226c29334388f7d44c8705a1d5361eabc4aa4cb
samins82-cloud <samins82@gmail.com>
2026-09-17 12:05:06 +0300

    Merge existing ELAK portal hosts into Okulum v0.4 WebView

--- elak-okulum/android/app/src/main/java/com/elak/okulum/MainActivity.kt
+++ elak-okulum/android/app/src/main/java/com/elak/okulum/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.webkit.CookieManager
 import android.webkit.WebChromeClient
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
@@ -13,6 +14,12 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     companion object {
         private const val BASE_URL = "https://elak.mcoaihl.com/okulum/"
+        private val INTERNAL_HOSTS = setOf(
+            "elak.mcoaihl.com",
+            "mcoaihl.com",
+            "www.mcoaihl.com",
+            "dp01.ni.net.tr"
+        )
     }
 
     private lateinit var webView: WebView
@@ -23,16 +30,28 @@ class MainActivity : AppCompatActivity() {
         webView = WebView(this)
         setContentView(webView)
 
-        webView.settings.javaScriptEnabled = true
-        webView.settings.domStorageEnabled = true
-        webView.settings.allowFileAccess = false
-        webView.settings.allowContentAccess = false
+        CookieManager.getInstance().setAcceptCookie(true)
+        CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true)
+
+        with(webView.settings) {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            databaseEnabled = true
+            allowFileAccess = false
+            allowContentAccess = false
+            loadsImagesAutomatically = true
+            userAgentString = "$userAgentString ELAK-Okulum/0.4"
+        }
+
         webView.webChromeClient = WebChromeClient()
         webView.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
                 val uri = request?.url ?: return false
-                val baseHost = Uri.parse(BASE_URL).host
-                if (uri.host == baseHost) return false
+                val scheme = uri.scheme?.lowercase().orEmpty()
+                val host = uri.host?.lowercase().orEmpty()
+
+                if ((scheme == "http" || scheme == "https") && host in INTERNAL_HOSTS) return false
+
                 return try {
                     startActivity(Intent(Intent.ACTION_VIEW, uri))
                     true
@@ -42,6 +61,11 @@ class MainActivity : AppCompatActivity() {
             }
         }
         webView.loadUrl(BASE_URL)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        CookieManager.getInstance().flush()
     }
 
     @Deprecated("Deprecated in Java")
