4b4cc65aca2fb825edf3e04f0e34bbde35fe90ba
samins82-cloud <samins82@gmail.com>
2026-09-17 12:28:11 +0300

    Bump ELAK Okulum to 0.4.1

--- elak-okulum/android/app/build.gradle.kts
+++ elak-okulum/android/app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.elak.okulum"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.4.1"
     }
 
     compileOptions {
