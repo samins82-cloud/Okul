4e790a18a97cf45577a2ab413a068d52fc7a2dff
samins82-cloud <samins82@gmail.com>
2026-09-18 17:54:14 +0300

    Route Akilli Rehber to v0.8.4 performance UI

--- elak-okulum/android/app/src/main/java/com/elak/okulum/rehber/RehberActivity81.kt
+++ elak-okulum/android/app/src/main/java/com/elak/okulum/rehber/RehberActivity81.kt
@@ -7,7 +7,9 @@ import androidx.appcompat.app.AppCompatActivity
 class RehberActivity81 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, RehberActivity82::class.java))
+        startActivity(Intent(this, RehberActivity84::class.java).apply {
+            intent.extras?.let { putExtras(it) }
+        })
         finish()
     }
 }
