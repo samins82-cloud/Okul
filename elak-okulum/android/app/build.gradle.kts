cff5f595d420672b9728a98b4ef01ed2e57f2ace
samins82-cloud <samins82@gmail.com>
2026-09-18 20:09:50 +0300

    Bump ELAK Okulum to v0.8.7 with Izin integration

--- elak-okulum/android/app/build.gradle.kts
+++ elak-okulum/android/app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.elak.okulum"
         minSdk = 24
         targetSdk = 35
-        versionCode = 22
-        versionName = "0.8.6"
+        versionCode = 23
+        versionName = "0.8.7"
     }
 
     compileOptions {
