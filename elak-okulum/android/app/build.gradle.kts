8b79e1b7c9a9afb9df18226822c8b721de84b859
samins82-cloud <samins82@gmail.com>
2026-09-18 17:55:04 +0300

    Bump ELAK Okulum to v0.8.4 performance release

--- elak-okulum/android/app/build.gradle.kts
+++ elak-okulum/android/app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.elak.okulum"
         minSdk = 24
         targetSdk = 35
-        versionCode = 19
-        versionName = "0.8.3"
+        versionCode = 20
+        versionName = "0.8.4"
     }
 
     compileOptions {
