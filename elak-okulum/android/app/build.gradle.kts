06c04b24fbabb4a7c971cf1be1c89a1bbf989fc6
samins82-cloud <samins82@gmail.com>
2026-09-18 08:26:44 +0300

    Bump ELAK Okulum to v0.7.1 Play Protect safe build

--- elak-okulum/android/app/build.gradle.kts
+++ elak-okulum/android/app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.elak.okulum"
         minSdk = 24
         targetSdk = 35
-        versionCode = 14
-        versionName = "0.7.0"
+        versionCode = 15
+        versionName = "0.7.1"
     }
 
     compileOptions {
