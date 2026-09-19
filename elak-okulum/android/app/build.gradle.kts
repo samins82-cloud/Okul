69e57bc79ee96c5408b2f792b32e6f7582ea4437
samins82-cloud <samins82@gmail.com>
2026-09-19 09:53:45 +0300

    Bump ELAK Okulum to 0.8.11 permanent-signing baseline

--- elak-okulum/android/app/build.gradle.kts
+++ elak-okulum/android/app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.elak.okulum"
         minSdk = 24
         targetSdk = 35
-        versionCode = 26
-        versionName = "0.8.10"
+        versionCode = 27
+        versionName = "0.8.11"
     }
 
     compileOptions {
