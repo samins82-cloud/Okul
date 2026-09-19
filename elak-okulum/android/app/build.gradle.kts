618d4bd6f2fab1b358991465a74cd2c2b341d86c
samins82-cloud <samins82@gmail.com>
2026-09-19 08:27:05 +0300

    Bump ELAK Okulum to 0.8.9 native izin

--- elak-okulum/android/app/build.gradle.kts
+++ elak-okulum/android/app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.elak.okulum"
         minSdk = 24
         targetSdk = 35
-        versionCode = 24
-        versionName = "0.8.8"
+        versionCode = 25
+        versionName = "0.8.9"
     }
 
     compileOptions {
