c24dfdefc3c21a8bdcfdd2d5298f50a5640b2359
samins82-cloud <samins82@gmail.com>
2026-09-21 21:58:06 +0300

    Bump ELAK CORE integration build

--- elak-okulum/android/app/build.gradle.kts
+++ elak-okulum/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
         applicationId = "com.elak.okulum"
         minSdk = 24
         targetSdk = 35
-        versionCode = 37
+        versionCode = 38
         versionName = "0.9.2"
     }
 
