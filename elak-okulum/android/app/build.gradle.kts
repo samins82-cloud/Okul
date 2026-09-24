60a03f7af2d654a15ccfc1a4d70915ca4ec647a3
samins82-cloud <samins82@gmail.com>
2026-09-24 07:01:34 +0300

    Bump ELAK Okulum to 0.9.5

--- elak-okulum/android/app/build.gradle.kts
+++ elak-okulum/android/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.elak.okulum"
         minSdk = 24
         targetSdk = 35
-        versionCode = 40
-        versionName = "0.9.4"
+        versionCode = 41
+        versionName = "0.9.5"
         manifestPlaceholders["appLabel"] = "ELAK Okulum"
     }
 
